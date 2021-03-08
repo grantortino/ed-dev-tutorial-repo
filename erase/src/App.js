2b1c3c74b63a506e2c4c8c6b65d1bc6ea5d90640
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-08 11:08:19 +0100

    set up

--- erase/src/App.js
+++ erase/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+
+  const items = [1, 2, 3, 4, 5];
+
+  const number = 5;
+
+  items.map((item) => {
+    console.log(item);
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="text">
+        <h1>HELLO</h1>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
