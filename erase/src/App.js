7dc9e9659973e4740381a175b7268417de016344
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-10 10:12:52 +0100

    completed events lesson

--- erase/src/App.js
+++ erase/src/App.js
@@ -8,12 +8,19 @@ function App() {
   const name = "Ed Dev";
   const message = "this is the actual tweet";
 
+// create functions
+  const sayHelloHandler = (e) => {
+    console.log(e);
+  };
+
   return (
     <div>
       <div className="text">
-        <h1>HELLO</h1>
-        <CreateTweet />
-        <TweetList name={name} message={message} />
+        {/* <h1>HELLO</h1> */}
+        {/* <CreateTweet /> */}
+        {/* <TweetList name={name} message={message} /> */}
+        <h1>Hello React</h1>
+        <button onClick={sayHelloHandler}>Click</button>
       </div>
     </div>
   );
