b1e478ecfc56a7681d681584dfef4dfc89fa970b
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-13 19:09:41 +0100

    added uuidv4

--- erase/src/components/Tweet.js
+++ erase/src/components/Tweet.js
@@ -3,14 +3,13 @@ import React from 'react';
 const Tweet = ({ name, tweet, setTweets, tweets }) => {
 
     const deleteTweet = () => {
-        console.log(tweet);
-        setTweets(tweets.filter((t) => t !== tweet));
+        setTweets(tweets.filter((state) => state.id !== tweet.id));
     };
 
     return (
         <div className="tweet">
             <h2>Name: {name} </h2>
-            <h3>message: {tweet}</h3>
+            <h3>message: {tweet.message}</h3>
             <button onClick={deleteTweet}>Delete</button>
             <button>Like</button>
         </div>
--- erase/src/components/TweetList.js
+++ erase/src/components/TweetList.js
@@ -5,7 +5,7 @@ const TweetList = ({ name, tweets, setTweets }) => {
 
     return (
         <div className="tweet-list">
-            {tweets.map((tweet) => <Tweet name={name} setTweets={setTweets} tweet={tweet} tweets={tweets}/>)}
+            {tweets.map((tweet) => <Tweet name={name} setTweets={setTweets} tweet={tweet} tweets={tweets} key={tweet.id} />)}
             {/* <Tweet name={name} /> */}
             {/* <Tweet name={name} message={message}/> */}
             {/* <Tweet name={name} message={message}/> */}
