05103e39cb8eb565ebc002d5ef8bd9e9f9f0bd71
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-14 11:47:58 +0100

    setting up music player

---
+++ music-player/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="music-player">Music Player</h1>
+    </div>
+  );
+}
+
+export default App;
