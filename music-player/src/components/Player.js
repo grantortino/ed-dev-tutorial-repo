20cec9724ecbb2430ad7be6729533a0871db90f0
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-14 16:40:14 +0100

    created player with pause  and play button

--- music-player/src/components/Player.js
+++ music-player/src/components/Player.js
@@ -1,8 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 // font awesome component
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // the actual icons
-import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faAngleLeft, faAngleRight, faPause } from '@fortawesome/free-solid-svg-icons';
 
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
@@ -10,7 +10,40 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     // reference to access the html tag "audio"
     const audioRef = useRef(null);
 
+    // Time format
+
+    const getTime = (time) => {
+        return (
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        );
+    };
+
+    // state
+    const [songInfo, setSongInfo] = useState({
+        currentTime: 0,
+        duration: 0
+    });
+
+    const timeUpdateHandler = (e) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({
+            ...songInfo,
+            currentTime: current,
+            duration: duration
+        })
+    };
+
     // Event Handlers
+
+    const dragHandler = (e) => {
+        audioRef.current.currentTime = e.target.value;
+        setSongInfo({
+            ...songInfo,
+            currentTime: e.target.value
+        });
+    };
+
     const playSongHandler = () => {
         if(isPlaying) {
             audioRef.current.pause();
@@ -24,16 +57,16 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     return(
         <div className="player">
             <div className="time-control">
-                <p>Start Time</p>
-                <input type="range"/>
-                <p>End Time</p>
+                <p>{getTime(songInfo.currentTime)}</p>
+                <input onChange={dragHandler} min={0} max={songInfo.duration} value={songInfo.currentTime} type="range"/>
+                <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" icon={ faAngleLeft } size="2x"/>
-                <FontAwesomeIcon onClick={playSongHandler} className="play" icon={ faPlay } size="2x" />
+                <FontAwesomeIcon onClick={playSongHandler} className="play" icon={ isPlaying ? faPause : faPlay } size="2x" />
                 <FontAwesomeIcon className="skip-forward" icon={ faAngleRight } size="2x"/>
             </div>
-            <audio ref={audioRef} src={currentSong.audio}></audio>
+            <audio onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
         </div>
     )
 };
--- music-player/src/components/Song.js
+++ music-player/src/components/Song.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Song = ({ currentSong }) => {
     return(
         <div className="song-container">
-            <img src={currentSong.cover} alt=""/>
+            <img src={currentSong.cover} alt={currentSong.name} />
             <h2>{currentSong.name}</h2>
             <h2>{currentSong.artist}</h2>
         </div>
